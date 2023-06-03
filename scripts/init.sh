207322e618eacc37d8c3ea17b4c4b6457cdb79bc
snkrheadz <aki.d.sc@gmail.com>
2023-06-03 20:58:19 +0900

    update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,8 +4,16 @@ if [ "$(uname)" != "Darwin" ] ; then
 	exit 1
 fi
 
+# Exit if xcode-select is not installed
+if ! which -s xcode-select; then
+  echo 'Install XCode first'
+  exit 1
+fi
+
 # Install xcode command line tools
 xcode-select --install > /dev/null
 
-# Install brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null
+# Install brew if not installed
+if ! which -s brew; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null
+fi
