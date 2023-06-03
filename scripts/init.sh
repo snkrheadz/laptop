51c9c3fb1e8e8154f200ef2a80fe137456a05bf3
snkrheadz <aki.d.sc@gmail.com>
2023-06-03 20:53:39 +0900

    install xcode command line tools and brew on init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+if [ "$(uname)" != "Darwin" ] ; then
+	echo "init.sh is designed for macOS"
+	exit 1
+fi
+
+# Install xcode command line tools
+xcode-select --install > /dev/null
+
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null
