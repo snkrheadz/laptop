02fcdd21bbfdaac7dd228792f36a5067427b13df
snkrheadz <aki.d.sc@gmail.com>
2023-08-28 13:40:06 +0900

    change go version to 1.21.0

--- scripts/install-go.sh
+++ scripts/install-go.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
-asdf install golang 1.20.3
-asdf global golang 1.20.3
+asdf install golang 1.21.0
+asdf global golang 1.21.0
