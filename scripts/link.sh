60af9132bbb861b3fc872c8b269c9c03f2cbd704
snkrheadz <aki.d.sc@gmail.com>
2024-01-17 18:14:51 +0900

    fix alacritty themes installation

--- scripts/link.sh
+++ scripts/link.sh
@@ -4,8 +4,8 @@ ln -sf $(pwd)/zsh $HOME/.zsh
 # dotfiles
 find dotfiles -name ".*" -type f | xargs -I{} ln -sf $(pwd)/{} $HOME
 # alacritty
-mkdir -p $HOME/.config/alacritty/themes
 if [ ! -d "$HOME/.config/alacritty/themes" ]; then
+    mkdir -p $HOME/.config/alacritty/themes
     git clone https://github.com/alacritty/alacritty-theme $HOME/.config/alacritty/themes
 fi
 if [ ! -f "$HOME/.config/alacritty/alacritty.toml" ]; then
