d384538cfe1e7b2cff7daa53d5f7eec74fb33ec8
snkrheadz <aki.d.sc@gmail.com>
2024-01-17 18:13:18 +0900

    add alacritty setup on link.sh

--- scripts/link.sh
+++ scripts/link.sh
@@ -3,3 +3,11 @@
 ln -sf $(pwd)/zsh $HOME/.zsh
 # dotfiles
 find dotfiles -name ".*" -type f | xargs -I{} ln -sf $(pwd)/{} $HOME
+# alacritty
+mkdir -p $HOME/.config/alacritty/themes
+if [ ! -d "$HOME/.config/alacritty/themes" ]; then
+    git clone https://github.com/alacritty/alacritty-theme $HOME/.config/alacritty/themes
+fi
+if [ ! -f "$HOME/.config/alacritty/alacritty.toml" ]; then
+    ln -sf $(pwd)/alacritty/alacritty.toml $HOME/.config/alacritty/alacritty.toml
+fi
